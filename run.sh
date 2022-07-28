00c4e5909ca99d8680099184af94ebbb70b3bc2c
FarbodPM <farbod.peimanzadeh@gmail.com>
2022-07-28 09:12:19 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -9,4 +9,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
